cca349e118df5e4e8525d78d8895f6ffef6ea269
kacperluk <kacper.luk@o2.pl>
2020-07-22 17:38:46 +0200

    Data Validation and Sanitization: Part II

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -8,62 +8,75 @@ mongoose.connect(connectionURL, {
     useCreateIndex: true
 })
 
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid')
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be positive number')
-            }
-        }
-    }
-})
+// const User = mongoose.model('User', {
+//     name: {
+//         type: String,
+//         required: true,
+//         trim: true
+//     },
+//     email: {
+//         type: String,
+//         required: true,
+//         trim: true,
+//         validate(value) {
+//             if (!validator.isEmail(value)) {
+//                 throw new Error('Email is invalid')
+//             }
+//         }
+//     },
+//     password: {
+//         type: String,
+//         required: true,
+//         validate(value) {
+//             if (value.includes('password') || value.length <= 6) {
+//                 throw new Error('Password too weak')
+//             }
+//         }
+//     },
+//     age: {
+//         type: Number,
+//         default: 0,
+//         validate(value) {
+//             if (value < 0) {
+//                 throw new Error('Age must be positive number')
+//             }
+//         }
+//     }
+// })
 
-const me = new User({
-    name: 'Rick',
-    age: 37,
-    email: 'rick.sanchez@ecorp.com'
-})
+// const me = new User({
+//     name: 'Rick',
+//     age: 37,
+//     password: 'sasdsdgfhrtqwe132',
+//     email: 'rick.sanchez@ecorp.com'
+// })
 
-me.save().then(() => {
-    console.log(me)
-}).catch((error) => {
-    console.log('Error!', error)
-})
+// me.save().then(() => {
+//     console.log(me)
+// }).catch((error) => {
+//     console.log('Error!', error)
+// })
 
 const Task = mongoose.model('Task', {
     description: {
-        type: String
+        type: String,
+        trim: true,
+        required: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     }
 })
 
-// const sometask = new Task({
-//     description: 'Wash the dishes',
-//     completed: false
-// })
+const sometask = new Task({
+    description: 'walk teh doggo',
+    completed: false
+})
 
-// sometask.save().then(() => {
-//     console.log(sometask)
-// }).catch((error) => {
-//     console.log(error)
-// })
+sometask.save().then(() => {
+    console.log(sometask)
+}).catch((error) => {
+    console.log(error)
+})
 
