64492f2552947a937f1ef7df06f9dbca4ec552e7
kacperluk <kacper.luk@o2.pl>
2020-07-22 14:13:13 +0200

    Inserting Documents

--- mongodb.js
+++ mongodb.js
@@ -13,14 +13,44 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
 
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({
-        name: 'Andrew',
-        age: 27
-    }, (error, result) => {
-        if (error) {
-            return console.log('Unable to insert user')
-        }
+    // db.collection('users').insertOne({
+    //     name: 'Andrew',
+    //     age: 27
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert user')
+    //     }
 
+    //     console.log(result.ops)
+    // })
+
+    // db.collection('users').insertMany([{
+    //     name: 'Jen',
+    //     age: 1232
+    // }, {
+    //     name: 'Gunther',
+    //     age: 1111
+    // }], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert documents!')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+    db.collection('tasks').insertMany([{
+        description: 'Learn mongodb',
+        completed: true
+    }, {
+        description: 'Walk the doggo',
+        completed: false
+    }, {
+        description: 'Sleep',
+        completed: true
+    }], (error, result) => {
+        if (error) {
+            return console.log('error')
+        }
         console.log(result.ops)
+        console.log(result.result)
     })
 })
