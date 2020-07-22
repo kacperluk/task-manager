f41ed844ced7bbc3f4f895f8ec2b96ee046133e4
kacperluk <kacper.luk@o2.pl>
2020-07-22 13:50:17 +0200

    first commit

---
+++ mongodb.js
@@ -0,0 +1,26 @@
+// CRUD creater read update delete
+
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-menager'
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to database')
+    }
+
+    const db = client.db(databaseName)
+
+    db.collection('users').insertOne({
+        name: 'Andrew',
+        age: 27
+    }, (error, result) => {
+        if (error) {
+            return console.log('Unable to insert user')
+        }
+
+        console.log(result.ops)
+    })
+})
