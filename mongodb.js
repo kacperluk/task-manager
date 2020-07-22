a61cc1005693e4bb49f900f374f786342ec7e2af
kacperluk <kacper.luk@o2.pl>
2020-07-22 14:48:08 +0200

    The ObjectID

--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,18 @@
 // CRUD creater read update delete
 
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+// const mongodb = require('mongodb')
+// const MongoClient = mongodb.MongoClient
+// const ObjectID = mongodb.ObjectID
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-menager'
+
+const id = new ObjectID()
+console.log(id.id)
+console.log(id.id.length)
+console.log(id.toHexString())
+console.log(id.toHexString().length)
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if (error) {
@@ -14,8 +22,8 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
     const db = client.db(databaseName)
 
     // db.collection('users').insertOne({
-    //     name: 'Andrew',
-    //     age: 27
+    //     name: 'Vikram',
+    //     age: 22
     // }, (error, result) => {
     //     if (error) {
     //         return console.log('Unable to insert user')
@@ -37,20 +45,20 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
 
     //     console.log(result.ops)
     // })
-    db.collection('tasks').insertMany([{
-        description: 'Learn mongodb',
-        completed: true
-    }, {
-        description: 'Walk the doggo',
-        completed: false
-    }, {
-        description: 'Sleep',
-        completed: true
-    }], (error, result) => {
-        if (error) {
-            return console.log('error')
-        }
-        console.log(result.ops)
-        console.log(result.result)
-    })
+    // db.collection('tasks').insertMany([{
+    //     description: 'Learn mongodb',
+    //     completed: true
+    // }, {
+    //     description: 'Walk the doggo',
+    //     completed: false
+    // }, {
+    //     description: 'Sleep',
+    //     completed: true
+    // }], (error, result) => {
+    //     if (error) {
+    //         return console.log('error')
+    //     }
+    //     console.log(result.ops)
+    //     console.log(result.result)
+    // })
 })
