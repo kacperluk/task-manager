b0416eafc0f212674d58c44e0c39af291f92a812
kacperluk <kacper.luk@o2.pl>
2020-07-23 15:29:41 +0200

    Resource Creation Endpoints: Part I

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
+
 const connectionURL = 'mongodb://127.0.0.1:27017/task-manager-api'
 
 mongoose.connect(connectionURL, {
@@ -7,55 +7,6 @@ mongoose.connect(connectionURL, {
     useUnifiedTopology: true,
     useCreateIndex: true
 })
-
-// const User = mongoose.model('User', {
-//     name: {
-//         type: String,
-//         required: true,
-//         trim: true
-//     },
-//     email: {
-//         type: String,
-//         required: true,
-//         trim: true,
-//         validate(value) {
-//             if (!validator.isEmail(value)) {
-//                 throw new Error('Email is invalid')
-//             }
-//         }
-//     },
-//     password: {
-//         type: String,
-//         required: true,
-//         validate(value) {
-//             if (value.includes('password') || value.length <= 6) {
-//                 throw new Error('Password too weak')
-//             }
-//         }
-//     },
-//     age: {
-//         type: Number,
-//         default: 0,
-//         validate(value) {
-//             if (value < 0) {
-//                 throw new Error('Age must be positive number')
-//             }
-//         }
-//     }
-// })
-
-// const me = new User({
-//     name: 'Rick',
-//     age: 37,
-//     password: 'sasdsdgfhrtqwe132',
-//     email: 'rick.sanchez@ecorp.com'
-// })
-
-// me.save().then(() => {
-//     console.log(me)
-// }).catch((error) => {
-//     console.log('Error!', error)
-// })
 
 const Task = mongoose.model('Task', {
     description: {
@@ -68,15 +19,3 @@ const Task = mongoose.model('Task', {
         default: false
     }
 })
-
-const sometask = new Task({
-    description: 'walk teh doggo',
-    completed: false
-})
-
-sometask.save().then(() => {
-    console.log(sometask)
-}).catch((error) => {
-    console.log(error)
-})
-
---
+++ src/models/user.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        validate(value) {
+            if (value.includes('password') || value.length <= 6) {
+                throw new Error('Password too weak')
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be positive number')
+            }
+        }
+    }
+})
+
+module.exports = User
