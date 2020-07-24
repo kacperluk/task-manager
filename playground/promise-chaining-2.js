48c55497fbf11c6f0a48a33a1fc6576bfee9b4c0
kacperluk <kacper.luk@o2.pl>
2020-07-24 11:10:15 +0200

    Async/Await

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,11 +1,24 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete('5f1971e8126fb92e97746527').then((task) => {
-    console.log(task)
-    return Task.find({ completed: true })
-}).then((tasks) => {
-    console.log('Tasks to complete:', tasks)
+// Task.findByIdAndDelete('5f1971e8126fb92e97746527').then((task) => {
+//     console.log(task)
+//     return Task.find({ completed: true })
+// }).then((tasks) => {
+//     console.log('Tasks to complete:', tasks)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const deleteTaskAndCount = async (_id, completed) => {
+    const deletedTask = await Task.findByIdAndDelete(_id)
+    console.log('Deleted Task:', deletedTask)
+    const tasks = await Task.find({ completed })
+    return tasks
+}
+
+deleteTaskAndCount('5f197219b2604b2eb575b299', true).then((count) => {
+    console.log('taskCount: ', count)
 }).catch((e) => {
-    console.log(e)
+    throw new Error('Somthing went wrong')
 })
--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -3,11 +3,24 @@ const User = require('../src/models/user')
 
 // 5f1972b3ff55aa2f19fdbf31
 
-User.findByIdAndUpdate('5f1972b3ff55aa2f19fdbf31', { age: 2312 }).then((user) => {
+// User.findByIdAndUpdate('5f1972b3ff55aa2f19fdbf31', { age: 2312 }).then((user) => {
+//     console.log(user)
+//     return User.countDocuments({ age: 2312 })
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const updateAgeAndCount = async (_id, age) => {
+    const user = await User.findByIdAndUpdate(_id, { age })
     console.log(user)
-    return User.countDocuments({ age: 2312 })
-}).then((result) => {
-    console.log(result)
+    const count = await User.countDocuments({ age })
+    return count
+}
+
+updateAgeAndCount('5f19894b887a8e4cf64d2ada', 1232131).then((count) => {
+    console.log(count)
 }).catch((e) => {
-    console.log(e)
+    console.log('e', e)
 })
