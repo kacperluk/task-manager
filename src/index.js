0415ca0f54c565f419df4bf58bd5ba40e46ad8e1
kacperluk <kacper.luk@o2.pl>
2020-07-26 16:35:30 +0200

    Securely Storing Passwords: Part I

--- src/index.js
+++ src/index.js
@@ -13,3 +13,17 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
+
+const bcrypt = require('bcryptjs')
+
+const myFunction = async () => {
+    const password = 'SOmePassWd'
+    const hashedPassword = await bcrypt.hash(password, 8)
+
+    console.log(password)
+    console.log(hashedPassword)
+    const isMatch = await bcrypt.compare(password, hashedPassword)
+    console.log(isMatch)
+}
+
+myFunction()
