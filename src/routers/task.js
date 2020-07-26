05971df524a7a62358196e9547b3fb0071f58636
kacperluk <kacper.luk@o2.pl>
2020-07-26 16:20:47 +0200

    Separate Route Files

---
+++ src/routers/task.js
@@ -0,0 +1,78 @@
+const express = require('express')
+const Task = require('../models/task')
+
+const router = new express.Router()
+
+
+router.post('/tasks', async (req, res) => {
+    const task = new Task(req.body)
+
+    try {
+        await task.save()
+        res.status(201).send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+router.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        res.send(tasks)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+router.get('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const task = await Task.findById(_id)
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+router.patch('/tasks/:id', async (req, res) => {
+    const allowedUpdates = ['completed', 'description']
+    const updates = Object.keys(req.body)
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    const _id = req.params.id
+
+    if (!isValidOperation) {
+        res.send(400).send({ error: 'Updates are invalid' })
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true })
+        res.send(task)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+
+})
+
+router.delete('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+    try {
+        const task = await Task.findByIdAndDelete(_id)
+
+        if (!task) {
+            res.status(404).send('Not found')
+        }
+
+        res.send(task)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+
+module.exports = router
