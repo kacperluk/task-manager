4e7488c702ef637ef2227b68cd4ed79c4547f12f
kacperluk <kacper.luk@o2.pl>
2020-07-28 18:33:59 +0200

    Accepting Authentication Tokens

--- src/index.js
+++ src/index.js
@@ -14,9 +14,9 @@ const port = process.env.PORT || 3000
 // })
 
 // Maitenance mode
-app.use((req, res, next) => {
-    res.status(503).send('Site is under maintenance')
-})
+// app.use((req, res, next) => {
+//     res.status(503).send('Site is under maintenance')
+// })
 
 app.use(express.json())
 app.use(userRouter)
