7b53b895985c2e81c26311ba1f0ccf3e6ce968f0
kacperluk <kacper.luk@o2.pl>
2020-07-28 17:03:13 +0200

    Generating Authentication Tokens

--- src/index.js
+++ src/index.js
@@ -14,16 +14,16 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
-const jwt = require('jsonwebtoken')
+// const jwt = require('jsonwebtoken')
 
-const bcrypt = require('bcryptjs')
+// const bcrypt = require('bcryptjs')
 
-const myFunction = async () => {
-    const token = jwt.sign({ _id: 'asdsdv123' }, 'thisisrandomsentence', { expiresIn: '7 days' })
-    console.log(token)
+// const myFunction = async () => {
+//     const token = jwt.sign({ _id: 'asdsdv123' }, 'thisisrandomsentence', { expiresIn: '7 days' })
+//     console.log(token)
 
-    const data = jwt.verify(token, 'thisisrandomsentence')
-    console.log(data)
-}
+//     const data = jwt.verify(token, 'thisisrandomsentence')
+//     console.log(data)
+// }
 
-myFunction()
+// myFunction()
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const User = require('../models/user')
-
 const router = new express.Router()
 
 router.post('/users', async (req, res) => {
@@ -8,7 +7,8 @@ router.post('/users', async (req, res) => {
 
     try {
         await user.save()
-        res.status(201).send(user)
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user, token })
     } catch (e) {
         res.status(400).send(e)
     }
@@ -17,7 +17,8 @@ router.post('/users', async (req, res) => {
 router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
-        res.send(user)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
     } catch (e) {
         res.status(400).send(e)
     }
