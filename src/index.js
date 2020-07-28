1224b89d3838df5f302ed1e611b1cb66e4206278
kacperluk <kacper.luk@o2.pl>
2020-07-28 17:42:33 +0200

    Express Middleware

--- src/index.js
+++ src/index.js
@@ -5,8 +5,20 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(express.json())
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//         res.send('GET requests are disabled')
+//     } else {
+//         next()
+//     }
+// })
 
+// Maitenance mode
+app.use((req, res, next) => {
+    res.status(503).send('Site is under maintenance')
+})
+
+app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
