8d0fae6c5e21c145c82b80943a39148918383c1a
kacperluk <kacper.luk@o2.pl>
2020-07-30 17:36:30 +0200

    Cascade Delete Tasks

--- src/index.js
+++ src/index.js
@@ -12,18 +12,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-const Task = require('./models/task')
-const User = require('./models/user')
-
-const main = async () => {
-    // const task = await Task.findById('5f22c8be309c361f06ff5c12')
-    // await task.populate('owner').execPopulate()
-    // console.log(task)
-
-    const user = await User.findById('5f22c88d309c361f06ff5c0f')
-    await user.populate('tasks').execPopulate()
-    console.log(user.tasks)
-}
-
-main()  
--- src/routers/task.js
+++ src/routers/task.js
@@ -19,20 +19,21 @@ router.post('/tasks', auth, async (req, res) => {
     }
 })
 
-router.get('/tasks', async (req, res) => {
+router.get('/tasks', auth, async (req, res) => {
     try {
-        const tasks = await Task.find({})
-        res.send(tasks)
+        await req.user.populate('tasks').execPopulate()
+        res.send(req.user.tasks)
     } catch (e) {
         res.status(500).send(e)
     }
 })
 
-router.get('/tasks/:id', async (req, res) => {
+router.get('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id
 
     try {
-        const task = await Task.findById(_id)
+        const task = await Task.findOne({ _id, owner: req.user._id })
+
         if (!task) {
             return res.status(404).send()
         }
@@ -43,19 +44,17 @@ router.get('/tasks/:id', async (req, res) => {
     }
 })
 
-router.patch('/tasks/:id', async (req, res) => {
+router.patch('/tasks/:id', auth, async (req, res) => {
     const allowedUpdates = ['completed', 'description']
     const updates = Object.keys(req.body)
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
-
-    const _id = req.params.id
 
     if (!isValidOperation) {
         res.send(400).send({ error: 'Updates are invalid' })
     }
 
     try {
-        const task = await Task.findById(_id)
+        const task = await Task.findOne({ _id: req.params.id, owner: req.user._id })
 
         if (!task) {
             res.status(404).send('Task not found')
@@ -71,10 +70,10 @@ router.patch('/tasks/:id', async (req, res) => {
 
 })
 
-router.delete('/tasks/:id', async (req, res) => {
+router.delete('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id
     try {
-        const task = await Task.findByIdAndDelete(_id)
+        const task = await Task.findOneAndDelete({ _id, owner: req.user._id })
 
         if (!task) {
             res.status(404).send('Not found')
