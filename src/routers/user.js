c781d10ab39e8980b070ae5e1ab98a2336d58d93
kacperluk <kacper.luk@o2.pl>
2020-07-30 13:32:14 +0200

    Authenticating User Endpoints

--- src/routers/user.js
+++ src/routers/user.js
@@ -52,59 +52,29 @@ router.get('/users/me', auth, async (req, res) => {
     res.send(req.user)
 })
 
-router.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
-
-    try {
-        const user = await User.findById(_id)
-        if (!user) {
-            return res.status(404).send('User not found')
-        }
-
-        return res.send(user)
-    } catch (e) {
-        res.status(500).send(e)
-    }
-})
-
-router.patch('/users/:id', async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'password', 'age']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
-
-    const _id = req.params.id
 
     if (!isValidOperation) {
         return res.status(400).send({ error: 'invalid updates!' })
     }
 
     try {
-        const user = await User.findById(_id)
-        // const user = await User.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true })
-
-        updates.forEach((update) => user[update] = req.body[update])
-
-        await user.save()
-        if (!user) {
-            res.status(404).send('not found bro')
-        }
-
-        res.send(user)
+        updates.forEach((update) => req.user[update] = req.body[update])
+        await req.user.save()
+        res.send(req.user)
     } catch (e) {
         res.status(400).send(e)
     }
 })
 
-router.delete('/users/:id', async (req, res) => {
-    const _id = req.params.id
+router.delete('/users/me', auth, async (req, res) => {
+    const _id = req.user._id
     try {
-        const user = await User.findByIdAndDelete(_id)
-
-        if (!user) {
-            return res.status(404).send()
-        }
-
-        res.send(user)
+        await req.user.remove()
+        res.send(req.user)
     } catch (e) {
         res.status(500).send(e)
     }
