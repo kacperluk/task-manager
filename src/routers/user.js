0a0d856b51a61b44dfb9065ac1669d0b6e36c4f1
kacperluk <kacper.luk@o2.pl>
2020-08-03 13:31:14 +0200

    Adding Images to User Profile

--- src/routers/user.js
+++ src/routers/user.js
@@ -53,26 +53,6 @@ router.get('/users/me', auth, async (req, res) => {
     res.send(req.user)
 })
 
-const upload = multer({
-    dest: 'avatars',
-    limits: {
-        fileSize: 1000000
-    },
-    fileFilter(req, file, cb) {
-        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-            return cb(new Error('Please upload one of the following file types: jpg, jpeg, png'))
-        }
-
-        cb(undefined, true)
-    }
-})
-
-router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
-    res.send()
-}, (error, req, res, next) => {
-    res.status(400).send({ error: error.message })
-})
-
 router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'password', 'age']
@@ -101,5 +81,33 @@ router.delete('/users/me', auth, async (req, res) => {
     }
 })
 
+const upload = multer({
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please upload one of the following file types: jpg, jpeg, png'))
+        }
+
+        cb(undefined, true)
+    }
+})
+
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    req.user.avatar = req.file.buffer
+    await req.user.save()
+    res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
+})
+
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
+})
 
 module.exports = router
