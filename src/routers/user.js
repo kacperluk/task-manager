dd313ea23663d13c143c3321f08666ce6da0b9ae
kacperluk <kacper.luk@o2.pl>
2020-08-03 12:36:39 +0200

    Validation Challange

--- src/routers/user.js
+++ src/routers/user.js
@@ -54,7 +54,17 @@ router.get('/users/me', auth, async (req, res) => {
 })
 
 const upload = multer({
-    dest: 'avatars'
+    dest: 'avatars',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please upload one of the following file types: jpg, jpeg, png'))
+        }
+
+        cb(undefined, true)
+    }
 })
 
 router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
