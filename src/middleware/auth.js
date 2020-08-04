c75cbcad88d0c0c80f502d96a4c69521c0780294
kacperluk <kacper.luk@o2.pl>
2020-08-04 13:18:44 +0200

    Fix authentication

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -14,7 +14,7 @@ const auth = async (req, res, next) => {
         req.user = user
         next()
     } catch (e) {
-        res.status(404).send({ error: 'Please authenticate', e: e })
+        res.status(404).send({ error: 'Please authenticate' })
     }
 }
 
