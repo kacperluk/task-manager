02b989ffdadbb5e11753d03e34779f09e88ae1d5
kacperluk <kacper.luk@o2.pl>
2020-08-04 15:01:46 +0200

    Writing Your Own Tests

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,4 +1,4 @@
-const { calculateTip } = require('../src/math')
+const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit } = require('../src/math')
 
 test('Should calculate total with tip', () => {
     const total = calculateTip(100, .3)
@@ -8,4 +8,14 @@ test('Should calculate total with tip', () => {
 test('Should calculate total with default tip', () => {
     const total = calculateTip(100)
     expect(total).toBe(110)
+})
+
+test('Should translate fahrenheit to celsius', () => {
+    const temp = fahrenheitToCelsius(32)
+    expect(temp).toBe(0)
+})
+
+test('Should translate celsius to fahrenheit', () => {
+    const temp = celsiusToFahrenheit(0)
+    expect(temp).toBe(32)
 })
