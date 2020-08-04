fe86df086c83eef93e1112e8d957ebdb5100e6df
kacperluk <kacper.luk@o2.pl>
2020-08-04 11:12:02 +0200

    Sending Welcome and Cancelation Emails

--- src/emails/account.js
+++ src/emails/account.js
@@ -2,9 +2,25 @@ const sgMail = require('@sendgrid/mail')
 const SENDGRID_API_KEY = require('../../sendgrid')
 sgMail.setApiKey(SENDGRID_API_KEY)
 
-sgMail.send({
-    to: 'kacper.luk@o2.pl',
-    from: 'kacper.luk@o2.pl',
-    subject: 'This is first email',
-    text: 'plzwork'
-})
+const sendWelcomeEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'kacper.luk@o2.pl',
+        subject: 'Thanks for joining in!',
+        text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
+    })
+}
+
+const sendCancelationEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'kacper.luk@o2.pl',
+        subject: 'Subscription Cancelation',
+        text: 'Your subscription was canelled successfully. Let us know what we can improve in our app'
+    })
+}
+
+module.exports = {
+    sendWelcomeEmail,
+    sendCancelationEmail
+}
--- src/routers/user.js
+++ src/routers/user.js
@@ -3,6 +3,7 @@ const User = require('../models/user')
 const auth = require('../middleware/auth')
 const multer = require('multer')
 const sharp = require('sharp')
+const { sendWelcomeEmail, sendCancelationEmail } = require('../emails/account')
 const router = new express.Router()
 
 router.post('/users', async (req, res) => {
@@ -10,6 +11,7 @@ router.post('/users', async (req, res) => {
 
     try {
         await user.save()
+        sendWelcomeEmail(user.email, user.name)
         const token = await user.generateAuthToken()
         res.status(201).send({ user, token })
     } catch (e) {
@@ -76,6 +78,7 @@ router.delete('/users/me', auth, async (req, res) => {
     const _id = req.user._id
     try {
         await req.user.remove()
+        sendCancelationEmail(req.user.email, req.user.name)
         res.send(req.user)
     } catch (e) {
         res.status(500).send(e)
