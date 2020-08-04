077040cfbfb6b32b87c797794e11ae0ba1ee83e6
kacperluk <kacper.luk@o2.pl>
2020-08-04 14:50:08 +0200

    Writing Tests and Assertions

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,7 +1,11 @@
-test('Hello world!', () => {
+const { calculateTip } = require('../src/math')
 
+test('Should calculate total with tip', () => {
+    const total = calculateTip(100, .3)
+    expect(total).toBe(130)
 })
 
-test('This should fail', () => {
-    throw new Error('Failure!')
+test('Should calculate total with default tip', () => {
+    const total = calculateTip(100)
+    expect(total).toBe(110)
 })
