c5e5e39fe2dbcb433986657a74b42d81d772384c
kacperluk <kacper.luk@o2.pl>
2020-08-05 17:34:33 +0200

    Testing with Task Data

--- src/routers/task.js
+++ src/routers/task.js
@@ -99,7 +99,7 @@ router.delete('/tasks/:id', auth, async (req, res) => {
         const task = await Task.findOneAndDelete({ _id, owner: req.user._id })
 
         if (!task) {
-            res.status(404).send('Not found')
+            return res.status(404).send('Not found')
         }
 
         res.send(task)
--- tests/task.test.js
+++ tests/task.test.js
@@ -1,7 +1,16 @@
 const request = require('supertest')
 const app = require('../src/app')
 const Task = require('../src/models/task')
-const { userOneId, userOne, setupDatbase } = require('./fixtures/db')
+const {
+    userOneId,
+    userOne,
+    userTwoId,
+    userTwo,
+    taskOne,
+    taskTwo,
+    taskThree,
+    setupDatbase
+} = require('./fixtures/db')
 
 beforeEach(setupDatbase)
 
@@ -13,7 +22,28 @@ test('Should create task for user', async () => {
             description: 'From my test'
         })
         .expect(201)
-    const task = Task.findById(response.body._id)
+    const task = await Task.findById(response.body._id)
     expect(task).not.toBeNull()
     expect(task.completed).toEqual(false)
+})
+
+test('Should get all tasks for userOne', async () => {
+    const response = await request(app)
+        .get('/tasks')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+
+    expect(response.body.length).toBe(2)
+})
+
+test('Should fail to delete task owned by another user', async () => {
+    const path = '/tasks/' + taskOne._id
+    await request(app)
+        .delete(path)
+        .set('Authorization', `Bearer ${userTwo.tokens[0].token}`)
+        .send()
+        .expect(404)
+    const task = await Task.findById(taskOne._id)
+    expect(task).not.toBeNull()
 })
